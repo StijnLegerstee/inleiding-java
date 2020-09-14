f85f156b7d5c67113fb848211d550d5a90b0e6a5
StijnLegerstee <slegerstee@roc-dev.com>
2020-09-14 14:37:15 +0200

    Added class Applet02

---
+++ src/h02/Applet02.java
@@ -0,0 +1,20 @@
+package h02;
+
+//Opdracht 2.1
+
+import java.awt.*;
+import java.applet.*;
+
+public class Applet02 extends Applet {
+
+    public void init() {
+        setBackground(Color.white);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.blue);
+        g.drawString("Stijn", 50, 60 );
+        g.setColor(Color.red);
+        g.drawString("Legerstee", 50, 80 );
+    }
+}
