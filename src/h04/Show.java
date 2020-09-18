33d2319f66a6a403afa127a13eb9dc91f04f8c55
StijnLegerstee <slegerstee@roc-dev.com>
2020-09-18 12:13:57 +0200

    Added and finished Opdracht 4.1

---
+++ src/h04/Show.java
@@ -0,0 +1,23 @@
+package h04;
+
+//Voorbeeld 4.1
+
+import java.awt.*;
+import java.applet.*;
+
+public class Show extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.black);
+        //Driehoek
+        g.setColor(Color.yellow);
+        g.drawLine(30,80,60,30);
+        g.setColor(Color.yellow);
+        g.drawLine(60, 30,90, 80);
+        g.setColor(Color.YELLOW);
+        g.drawLine(90,80, 30, 80);
+    }
+}
