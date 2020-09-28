9dc83ae0774e195bd7ee1504fe8671a2a492abfa
StijnLegerstee <slegerstee@roc-dev.com>
2020-09-28 23:29:07 +0200

    net bezig met h08

---
+++ src/h08/voorbeeld8_1.java
@@ -0,0 +1,21 @@
+package h08;
+
+//Voorbeeld 8.1
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class voorbeeld8_1 extends Applet {
+    Button knop;
+
+    public void init() {
+        knop = new Button();
+        knop.setLabel( "Klik op mij" );
+        add(knop);
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("Doet deze knop wel iets?", 50, 60 );
+    }
+}
